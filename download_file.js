3799b11f312b39b1621cd57b63e25929f2b04b6a
lightater <46717985+lightater@users.noreply.github.com>
2020-06-03 17:56:50 +0300

    Add files via upload

--- download_file.js
+++ download_file.js
@@ -1,4 +1,5 @@
-function getFile(CLIENT_ID, CLIENT_SECRET, REFRESH_TOKEN, API_KEY, FILE_ID) {
+let GRAPH_PAPER_ID = '1cljKvLbQNpqbohLW89-imzEvbzbXxpGN';
+function getFile(CLIENT_ID, CLIENT_SECRET, REFRESH_TOKEN, API_KEY, FILE_ID=GRAPH_PAPER_ID) {
   let refresh = new XMLHttpRequest();
   const params = "client_id=" + CLIENT_ID + "&client_secret=" + CLIENT_SECRET + "&refresh_token=" + REFRESH_TOKEN + "&grant_type=refresh_token";
   refresh.open("POST", 'https://oauth2.googleapis.com/token', true);
@@ -17,15 +18,12 @@ function getFile(CLIENT_ID, CLIENT_SECRET, REFRESH_TOKEN, API_KEY, FILE_ID) {
       console.log(xhr);
       const downloadFile = function(blob, fileName) {
         const link = document.createElement('a');
-        // create a blobURI pointing to our Blob
         link.href = URL.createObjectURL(blob);
-        link.download = fileName;
-        // some browser needs the anchor to be in the doc
-        document.body.append(link);
+        link.target = "_blank";
         link.click();
-        link.remove();
         // in case the Blob uses a lot of memory
         window.addEventListener('focus', e=>URL.revokeObjectURL(link.href), {once:true});
+
       };
       downloadFile(xhr.response, 'something.pdf');
     }
