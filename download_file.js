f815a02309d8c549ac7608d5ec73167088df5b5f
lightater <46717985+lightater@users.noreply.github.com>
2020-06-04 07:10:55 +0300

    Add files via upload

--- download_file.js
+++ download_file.js
@@ -1,5 +1,5 @@
 let GRAPH_PAPER_ID = '1cljKvLbQNpqbohLW89-imzEvbzbXxpGN';
-function getFile(CLIENT_ID, CLIENT_SECRET, REFRESH_TOKEN, API_KEY, FILE_ID=GRAPH_PAPER_ID) {
+function getUrl(CLIENT_ID, CLIENT_SECRET, REFRESH_TOKEN, API_KEY, FILE_ID, problem_dataurl) {
   let refresh = new XMLHttpRequest();
   const params = "client_id=" + CLIENT_ID + "&client_secret=" + CLIENT_SECRET + "&refresh_token=" + REFRESH_TOKEN + "&grant_type=refresh_token";
   refresh.open("POST", 'https://oauth2.googleapis.com/token', true);
@@ -7,7 +7,7 @@ function getFile(CLIENT_ID, CLIENT_SECRET, REFRESH_TOKEN, API_KEY, FILE_ID=GRAPH
   refresh.responseType = 'json';
   refresh.send(params);
   refresh.onload = function(response) {
-    console.log(refresh.response);
+    //console.log(refresh.response);
     let accessToken = refresh.response.access_token;
 
     let xhr = new XMLHttpRequest();
@@ -15,17 +15,12 @@ function getFile(CLIENT_ID, CLIENT_SECRET, REFRESH_TOKEN, API_KEY, FILE_ID=GRAPH
     xhr.open("GET", "https://www.googleapis.com/drive/v3/files/"+ FILE_ID +'?&alt=media' +'&key=' + API_KEY, true);
     xhr.setRequestHeader('Authorization','Bearer '+ accessToken);
     xhr.onload = function(){
-      console.log(xhr);
-      const downloadFile = function(blob, fileName) {
-        const link = document.createElement('a');
-        link.href = URL.createObjectURL(blob);
-        link.target = "_blank";
-        link.click();
-        // in case the Blob uses a lot of memory
-        window.addEventListener('focus', e=>URL.revokeObjectURL(link.href), {once:true});
-
-      };
-      downloadFile(xhr.response, 'something.pdf');
+      //console.log(xhr);
+      /*window.open(URL.createObjectURL(xhr.response));
+      let dataurl = URL.createObjectURL(xhr.response);
+      console.log(URL.createObjectURL(xhr.response));*/
+      console.log(xhr.response);
+      problem_dataurl.dataurl = xhr.response;
     }
     xhr.send();
   }
